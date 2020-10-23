e3bb91d39e3bb46a27d67533f674f88c81ae6bc6
Brandenberger Stefan stb <Stefan.Brandenberger@apgsga.ch>
2020-10-23 14:28:32 +0200

    show subjOid and gepardNr in vaadin app

--- vaadin8-app/src/main/java/ch/apg/sso/user/dto/AccountDTO.java
+++ vaadin8-app/src/main/java/ch/apg/sso/user/dto/AccountDTO.java
@@ -5,75 +5,90 @@ import java.util.Set;
 import ch.apg.sso.security.AuthorityConstant;
 
 public class AccountDTO {
-    private String id;
+	private String id;
 
-    private String username;
+	private String username;
 
-    private String firstName;
+	private String firstName;
 
-    private String lastName;
+	private String lastName;
 
-    private String email;
+	private String email;
 
-    private Set<AuthorityConstant> authorities;
+	private String subjOid;
+	
+	private String gepardNr;
 
-    public String getId() {
-        return id;
-    }
+	private Set<AuthorityConstant> authorities;
 
-    public void setId(String id) {
-        this.id = id;
-    }
+	public String getId() {
+		return id;
+	}
 
-    public String getUsername() {
-        return username;
-    }
+	public void setId(String id) {
+		this.id = id;
+	}
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
+	public String getUsername() {
+		return username;
+	}
 
-    public String getFirstName() {
-        return firstName;
-    }
+	public void setUsername(String username) {
+		this.username = username;
+	}
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
+	public String getFirstName() {
+		return firstName;
+	}
 
-    public String getLastName() {
-        return lastName;
-    }
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
+	public String getLastName() {
+		return lastName;
+	}
 
-    public String getEmail() {
-        return email;
-    }
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+	public String getEmail() {
+		return email;
+	}
 
-    public Set<AuthorityConstant> getAuthorities() {
-        return authorities;
-    }
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
-    public void setAuthorities(Set<AuthorityConstant> authorities) {
-        this.authorities = authorities;
-    }
+	public Set<AuthorityConstant> getAuthorities() {
+		return authorities;
+	}
 
-    @Override
-    public String toString() {
-        return "AccountDTO{" +
-               "id='" + id + '\'' +
-               ", username='" + username + '\'' +
-               ", firstName='" + firstName + '\'' +
-               ", lastName='" + lastName + '\'' +
-               ", email='" + email + '\'' +
-               ", authorities=" + authorities +
-               '}';
-    }
+	public void setAuthorities(Set<AuthorityConstant> authorities) {
+		this.authorities = authorities;
+	}
+
+	public String getSubjOid() {
+		return subjOid;
+	}
+
+	public void setSubjOid(String subjOid) {
+		this.subjOid = subjOid;
+	}
+
+	public String getGepardNr() {
+		return gepardNr;
+	}
+
+	public void setGepardNr(String gepardNr) {
+		this.gepardNr = gepardNr;
+	}
+
+	@Override
+	public String toString() {
+		return "AccountDTO [id=" + id + ", username=" + username + ", firstName=" + firstName + ", lastName=" + lastName
+				+ ", email=" + email + ", subjOid=" + subjOid + ", gepardNr=" + gepardNr + ", authorities="
+				+ authorities + "]";
+	}
 }
